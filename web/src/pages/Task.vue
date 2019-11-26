8a7403e302f5068feb90626a48aac515fdc7f1b3
Roay <roachard@hotmail.com>
2019-11-26 21:58:39 +0800

    front-end optimization

--- web/src/pages/Task.vue
+++ web/src/pages/Task.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="wrapper">
-    <h1>您需要使用一次 ExinEarn 才能加入 ExinEarn 中文群</h1>
-    <h2>搜索 ExinEarn 机器人: 7000000014，不花钱定投比特币</h2>
+    <h2>您需要使用一次 ExinEarn 才能加入 ExinEarn 中文群</h2>
+    <h3>搜索 ExinEarn 机器人: 7000000014，不花钱定投比特币</h3>
+    <h3><a href="https://support.exinone.com/hc/zh-cn/articles/360035568272">操作流程请点击</a></h3>
   </div>
 </template>
 
@@ -14,9 +15,14 @@ export default {
 
 <style scoped>
 .wrapper {
-  background: gold;
+  background: #ce0e2f;
   padding: 5vw;
   color: white;
   height: 100%;
+}
+
+.wrapper a {
+  color: white;
+  text-decoration: underline;
 }
 </style>
